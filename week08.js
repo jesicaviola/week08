72484d4952a3e3fe702b458b687bb3d13382dd44
Jesica Viola <jesica.viola@gmail.com>
2021-03-27 11:08:38 -0300

    exercise 1b

--- week08.js
+++ week08.js
@@ -1,4 +1,11 @@
 var x = 1;
 var y = 3;
 var z = x + y;
-console.log(z)
+console.log(z);
+
+var firstname = "Jesica";
+var lastname = "Viola";
+var CompleteName = firstname + lastname;
+console.log(CompleteName);
+
+
