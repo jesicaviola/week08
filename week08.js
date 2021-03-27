ddbc4d18d02bfdcf671740995bad330562027500
Jesica Viola <jesica.viola@gmail.com>
2021-03-27 18:04:39 -0300

    exercise 2f

--- week08.js
+++ week08.js
@@ -21,16 +21,24 @@ var text4 = "create a new string";
 var NewString = text4.substring(0, 5);
 console.log(NewString);
 
-var text5 = "create a new string";
-var NewString2 = text5.substring(16,19);
+var text5 = "create a new string last 3";
+var NewString2 = text5.substring(text5.length-3);
 console.log(NewString2);
 
-var text6 = "create a new string";
-var NewString3 = text6.charAt(0).toUpperCase() + text6.substring(1,19).toLowerCase();
+var text6 = "create a new string capitalize";
+var NewString3 = text6.charAt(0).toUpperCase() + text6.substring(1).toLowerCase();
 console.log(NewString3);
 
 var text7 = "create a new string";
 var newString4 = text7.indexOf(" ");
 console.log(newString4);
 
+var text8 = "uppercase lowercase";
+var space = text8.indexOf(" ");
+var text8a = text8.substring(0,space);
+var text8b = text8.substring(space+1);
+var text8a1 = text8a.charAt(0).toUpperCase() + text8a.substring(1).toLowerCase()
+var text8b1 = text8b.charAt(0).toUpperCase() + text8b.substring(1).toLowerCase()
+var newString5 = text8a1 + " " + text8b1;
+console.log(newString5);
 
